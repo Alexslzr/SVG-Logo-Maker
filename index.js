91452ff664b8beba0223615a1780d471ed87e2af
Alejandro Salazar <alejandroslzr17@gmail.com>
2023-05-20 13:24:51 -0700

    Initial structuring of the repo and inquirer questions

---
+++ index.js
@@ -0,0 +1,45 @@
+const inquirer = require('inquirer')
+const fs = require('fs')
+
+const settings = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'please enter initials',
+        default: 'No more than 3 characters'
+    },
+    {
+        type: 'input',
+        name: 'colortitle',
+        message: 'please enter color for the title',
+        default: 'can be also in Hexadecimal'
+    },
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'please choose the shape of your Logo',
+        choices: ['circle', 'triangle', 'square']
+    },
+    {
+        type: 'input',
+        name: 'shapecolor',
+        message: 'please enter color for the shape (the background)',
+        default: 'can be also in Hexadecimal'
+    },
+]
+
+
+
+function init(){
+inquirer
+    .prompt(settings)
+    .then((answers)=>{
+        const readMeContent = generateReadMe(answers);
+        fs.writeFile('logo.svg', readMeContent, (err) =>
+        err ? console.log(err) : console.log('Generated logo.svg')
+        );
+    })
+
+}
+
+init();
